0445e73b61a0f001e67cf6c3185237a609e2ae9d
Terross <dima38091@gmail.com>
2022-11-13 15:44:07 +0300

    init

---
+++ src/main/java/ru/leti/distributed/application/labs/DistributedApplicationLabsApplication.java
@@ -0,0 +1,13 @@
+package ru.leti.distributed.application.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedApplicationLabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedApplicationLabsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/leti/distributed/application/labs/DistributedApplicationLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.leti.distributed.application.labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedApplicationLabsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
